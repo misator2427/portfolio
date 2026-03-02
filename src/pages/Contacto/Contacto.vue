f89a38482c1ab5dc7b2c1db27af3ac3d9b96c8b4
Miquel Satorre <misator2427gmail.com>
2026-03-02 18:19:35 +0100

    Página inicial con estructura adecuada, modificar detalles

--- src/pages/Contacto/Contacto.vue
+++ src/pages/Contacto/Contacto.vue
@@ -1,3 +1,51 @@
+
+<script setup lang="ts">
+
+</script>
+
 <template>
-    <h1>Contacto page</h1>
+
+    <section class="h-200 bg-amber-200" > 
+      <h1> DONDE </h1>  
+
+      <div class="container mx-auto max-w-5xl px-4">
+
+            <h2 class="text-3xl font-bold text-gray-900 mb-8 text-center">Contacta con Batman</h2>
+            
+            <div class="flex flex-col lg:flex-row gap-8 items-center">
+
+                <!-- Mapa de Google Maps -->
+                <div class="hidden lg:block w-full lg:w-1/2 rounded-lg overflow-hidden shadow-lg">
+                    <div class="aspect-square">
+                    <iframe 
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3115.3997258892937!2d-0.3996366!3d38.66268065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6189db351339f9%3A0xb34d9f49ad745484!2s03816%20Benifallim%2C%20Alicante!5e0!3m2!1ses!2ses!4v1771242323082!5m2!1ses!2ses" 
+                    width="600" 
+                    height="450" 
+                    class="w-full h-full rounded-lg"
+                    :style="{border:0}"
+                    allowfullscreen
+                    loading="lazy" 
+                    referrerpolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                    </div>
+                </div>
+
+            </div>  <!-- Formulario -->
+        </div>
+    
+    
+    </section>
+
+        
+        
+    
+
+    <section class="h-200 bg-blue-200"  > Mis redes </section>
+
 </template>
+
+
+
+<style scoped>
+
+</style>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,57 +1,175 @@
 <script lang="ts" setup>
+import Button from '@/components/ui/button/Button.vue';
+
+import Card from '@/components/ui/card/Card.vue';
+import CardContent from '@/components/ui/card/CardContent.vue';
+
 
 
 </script>
 
 <template>
 
-    <div></div>
+     <div
+        class="min-h-screen w-full text-white"
+        style="
+            background-image: url('/Imágenes/Benifallim/Benifallim.webp');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        "
+    >
 
-    <div class= "bg-amber-200 flex h-200">
-        <header class="presentación">
+        <!-- OVERLAY OSCURO -->
+        <div class="min-h-screen w-full bg-black/40">
+    
 
-            <img class="absolute opacity-20 h-200 w-full" src="/Imágenes/Benifallim/Benifallim.webp" alt="">
+            <div class="h-screen p-10 flex items-end">
+                <header class="presentación w-full">
 
-            <h1 class="text-5xl md:text-7xl lg:text-8xl font-bold pb-5 md:pb-15 z-10 transition-all">
-                <span class="text-[rgb(27,182,246)] ">Miquel</span> 
-                <br> 
-                <!-- br es un salto de línea -->
-                Satorre <span class="text-[rgb(27,182,246)] ">Santonja</span>
-            </h1>
+                    <h1 class="text-2xl md:text-7xl lg:text-8xl font-bold md:pb-15 z-10 transition-all">
+                        <span>Miquel</span>
+                        <br>
+                        <!-- br es un salto de línea -->
+                        <span>Satorre Santonja</span>
 
+                        <p class="text-lg md:text-3xl transition-all mt-4">
+                            Esta es una web de presentación de Miquel Satorre Santonja.
+                        </p>
+                    </h1>
 
-            <p class="text-lg md:text-3xl transition-all">
-                Nini y poco mas
-            </p>
-        </header>
-    </div>
-<!-- aqi yo tengo mis rutas, y donde van viene marcado por el router link, el resto son creando los botones. Ojo que coincida el nombre que le he pueesto en el index con el que llamo -->
-    <div class= "bg-blue-200 flex h-200 items-center row justify-center">
-        <header class="botones">
-            <div class="z-10 grid grid-cols-2 gap-4 items-center text-2xl">
-                <Button variant="destructive" class="mr-1 bg-[rgb(27,182,246)] md:py-5 md:px-6 md:text-lg hover:bg-[rgba(24,182,0.7)] transition-all">
-                <RouterLink to="/otros"> Otros </RouterLink>
-                </Button>
-
-                <Button variant="destructive" class="mr-1 bg-[rgb(27,182,246)] md:py-5 md:px-6 md:text-lg hover:bg-[rgba(24,182,0.7)] transition-all">
-                <RouterLink to="/trabajos"> Trabajos </RouterLink>
-                </Button>
-
-                <Button variant="destructive" class="mr-1 bg-[rgb(27,182,246)] md:py-5 md:px-6 md:text-lg hover:bg-[rgba(24,182,0.7)] transition-all">
-                <RouterLink to="/datos"> Datos </RouterLink>
-                </Button>
-                
-                 <Button variant="destructive" class="mr-1 bg-[rgb(27,182,246)] md:py-5 md:px-6 md:text-lg hover:bg-[rgba(24,182,0.7)] transition-all">
-                <RouterLink to="/contacto"> Contacto </RouterLink>
-                </Button>
-                
+                </header>
             </div>
-
             
-        </header>
+            
+            <!-- aqi yo tengo mis rutas, y donde van viene marcado por el router link, el resto son creando los botones. Ojo que coincida el nombre que le he pueesto en el index con el que llamo -->
+            <div class= " flex h-screen items-center row justify-center">
+                <header class="botones">
+                    <div class="z-10 grid grid-cols-2 gap-x-150 gap-y-50 items-center text-2xl ">
+
+                        
+                        <!-- tarjeta de otros -->
+                        <Card 
+                            class="cursor-pointer w-[220px] h-[240px] mr-1 bg-[rgb(27,182,246)] md:py-5 md:px-6 md:text-lg hover:bg-[rgba(24,182,0.7)] transition-all hover:scale-105"
+                            @click="$router.push(`/otros`)"
+                        >
+
+                            <CardContent class="flex flex-col items-center justify-start h-full pt-0.5">
+
+                                <h2 class="text-xl font-semibold text-center"> 
+                                    Otros 
+                                </h2>
+
+                                <div class="w-full flex justify-center">
+                                    <img 
+                                        src="/Imágenes/Nah.webp"
+                                        alt="Otros"
+                                        class="max-w-full max-h-full object-contain rounded-md"
+                                    >
+                                </div>
+
+ 
+
+                            </CardContent>
+
+                        </Card>
+
+                        <!-- tarjeta de trabajos -->
+                        <Card 
+                            class="cursor-pointer w-[220px] h-[240px] mr-1 bg-[rgb(27,182,246)] md:py-5 md:px-6 md:text-lg hover:bg-[rgba(24,182,0.7)] transition-all hover:scale-105"
+                            @click="$router.push(`/trabajos`)"
+                        >
+
+                            <CardContent class="flex flex-col items-center justify-start h-full pt-0.5">
+
+                                
+                                <h2 class="text-xl font-semibold text-center"> 
+                                    Trabajos 
+                                </h2>
+
+                                <div class="w-full flex justify-center">
+                                    <img 
+                                        src="/Imágenes/Marshall.webp"
+                                        alt="Trabajos"
+                                        class="max-w-full max-h-full object-contain rounded-md"
+                                    >
+                                </div>
+
+                            </CardContent>
+
+                        </Card>
+
+                        <!-- tarjeta de datos -->
+                        <Card 
+                            class="cursor-pointer w-[220px] h-[240px] mr-1 bg-[rgb(27,182,246)] md:py-5 md:px-6 md:text-lg hover:bg-[rgba(24,182,0.7)] transition-all hover:scale-105"
+                            @click="$router.push(`/datos`)"
+                        >
+
+                            <CardContent class="flex flex-col items-center justify-start h-full pt-0.5">
+
+                                
+
+                                <h2 class="text-xl font-semibold text-center"> 
+                                    Datos 
+                                </h2>
+
+                                <div class="w-full flex justify-center">
+                                    <img 
+                                        src="/Imágenes/Speed.jpg"
+                                        alt="Datos"
+                                        class="max-w-full max-h-full object-contain rounded-md"
+                                    >
+                                </div>
+
+                            </CardContent>
+
+                        </Card>
+
+                        <!-- tarjeta de contacto -->
+
+                        <Card 
+                            class="cursor-pointer w-[220px] h-[240px] mr-1 bg-[rgb(27,182,246)] md:py-5 md:px-6 md:text-lg hover:bg-[rgba(24,182,0.7)] transition-all hover:scale-105"
+                            @click="$router.push(`/contacto`)"
+                        >
+
+                            <CardContent class="flex flex-col items-center justify-start h-full pt-0.5">
+
+                               
+
+                                <h2 class="text-xl font-semibold text-center">
+                                    Contacto
+                                </h2>
+
+                                <div class="w-full flex justify-center">
+                                    <img 
+                                        src="/Imágenes/Ballin.jpg"
+                                        alt="Contacto"
+                                        class="max-w-full max-h-full object-contain rounded-md"
+                                    >
+                                </div>
+
+                            </CardContent>
+
+                        </Card>
+
+                        
+
+                        
+
+                        
+
+                        
+                        
+                        
+                        
+                    </div>
+
+                    
+                </header>
+            </div>
+        </div>
     </div>
-
-
 </template>
 
 <style scoped>
