d43aa4aba9f189ef3c5f4e7c6ca37930eb01c380
Miquel Satorre <misator2427gmail.com>
2026-03-04 13:07:25 +0100

    Página de Contacto con la estructura y elementos correctos, hay que cambiar el diseño solo

--- src/pages/Contacto/Contacto.vue
+++ src/pages/Contacto/Contacto.vue
@@ -18,6 +18,8 @@ import {
   PopoverTrigger,
 } from '@/components/ui/popover'
 
+import { Phone, Mail, MapPin, X, Instagram, Linkedin, InstagramIcon, MessageCircle, Menu, MenuIcon } from 'lucide-vue-next';
+
 
 const dias = ref<DateValue> ()
 
@@ -57,11 +59,22 @@ const dias = ref<DateValue> ()
 
   <div class="container mx-auto max-w-5xl px-4">
 
-    <h2 class="text-3xl font-bold text-gray-900 mb-8 text-center">Contacta con Batman</h2>
+   
           
-    <div class="flex lg:flex-row gap-8 items-center">
+    <div class="flex lg:flex-row gap-8 ">
 
-            <!-- Mapa de Google Maps -->
+      <h1 class="text-1xl md:text-7xl lg:text-8xl font-bold md:pb-15 z-10 transition-all">
+        <span>Miquel</span>
+        <br>
+        <!-- br es un salto de línea -->
+        <span>Satorre Santonja</span>
+
+        <p class="text-lg md:text-3xl transition-all mt-4">
+            Esta es una web de presentación de Miquel Satorre Santonja.
+        </p>
+      </h1>
+
+      <!-- Mapa de Google Maps -->
       <div class="hidden lg:block w-full lg:w-1/2 rounded-lg overflow-hidden shadow-lg">
         <div class="aspect-square">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24910.002153592683!2d-0.4990140953267379!3d38.70058034303596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd61864e204bb377%3A0x3270bc5ab4510472!2sAlcoy%2C%20Alicante!5e0!3m2!1ses!2ses!4v1772616610408!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
@@ -73,84 +86,95 @@ const dias = ref<DateValue> ()
   </div>
 </section>
 
-<section id="Contacto" class="w-full py-12 bg-gray-100 h-screen">
-  <div class="container mx-auto max-w-5xl px-4">
+<section id="Contacto" class="w-full py-12 bg-gray-100 min-h-screen flex items-center">
+  <div class="container mx-auto max-w-5xl px-4 flex flex-col lg:flex-row items-start gap-12">
+
+    <h1 class="order-1 lg:order-2 w-full lg:w-1/2 text-right lg:text-left text-3xl md:text-7xl lg:text-8xl font-bold transition-all">
+        <span>Miquel</span>
+        <br>
+        <!-- br es un salto de línea -->
+        <span>Satorre Santonja</span>
+
+        <p class="text-lg md:text-3xl transition-all mt-4">
+            Esta es una web de presentación de Miquel Satorre Santonja.
+        </p>
+    </h1>
 
 
-        <!-- Formulario -->
-        <div class="w-full max-w-md mx-auto lg:max-w-none lg:w-1/2 ">
-          <form  class="space-y-6 bg-white p-8 rounded-lg shadow-lg aspect-square">
-            <div class="space-y-2">
-                  <Label for="nomen">Nombre</Label>
-                  <Input id="nomen" required /> 
-                  
+<!-- Formulario -->
+    <div class="order-2 lg:order-1 w-full lg:w-1/2">
+      <form  class="space-y-6 bg-white p-8 rounded-2xl shadow-xl w-full">
+        <div class="space-y-2">
+              <Label for="nomen">Nombre</Label>
+              <Input id="nomen" required /> 
+              
 
-            </div>
-                  
-            <div class="space-y-2">
-                  <Label for="cognomen">Apellidos</Label>
-                  <Input id="cognomen" required /> 
-
-            </div>
-
-            <div class="space-y-2">
-              <Select required>
-                <SelectTrigger class="border-gray-200 bg-white text-gray-900">
-                  <SelectValue placeholder="Selecciona una misión" />
-                </SelectTrigger>
-
-
-                <SelectContent class="bg-white">
-                  <SelectItem value="rescatare">
-                    Rescate de rehenes
-                  </SelectItem>
-                  <SelectItem value="investigare">
-                    Investigacion criminal
-                  </SelectItem>
-                  <SelectItem value="persequi">
-                    Persecucion de villanos
-                  </SelectItem>
-                  <SelectItem value="defendere">
-                    Defensa de Gotham
-                  </SelectItem>
-                  
-                </SelectContent>
-              </Select>
-
-            </div>
-
-            <div class="space-y-2">
-              <Label>Fecha de la mision</Label>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button variant="outline">
-                    <span v-if="dias"> 
-                      {{dias.day}} / {{ dias.month }} / {{ dias.year }}
-                    </span>
-
-                    <span v-else> 
-                      Selecciona una fecha 
-                    </span>
-
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent>  
-                  <Calendar v-model="dias"/>
-                </PopoverContent>
-              </Popover>
-
-            </div>
-
-            <Button
-            type="submit"
-            class="w-full bg-[rgb(106,90,205)] hover:bg-[rgb(88,75,171)] text-white text-md mt-4"
-            >
-              Enviar solicitud
-            </Button>
-
-                
-          </form>
         </div>
+              
+        <div class="space-y-2">
+              <Label for="cognomen">Apellidos</Label>
+              <Input id="cognomen" required /> 
+
+        </div>
+
+        <div class="space-y-2">
+          <Select required>
+            <SelectTrigger class="border-gray-200 bg-white text-gray-900">
+              <SelectValue placeholder="Selecciona una misión" />
+            </SelectTrigger>
+
+
+            <SelectContent class="bg-white">
+              <SelectItem value="rescatare">
+                Rescate de rehenes
+              </SelectItem>
+              <SelectItem value="investigare">
+                Investigacion criminal
+              </SelectItem>
+              <SelectItem value="persequi">
+                Persecucion de villanos
+              </SelectItem>
+              <SelectItem value="defendere">
+                Defensa de Gotham
+              </SelectItem>
+              
+            </SelectContent>
+          </Select>
+
+        </div>
+
+        <div class="space-y-2">
+          <Label>Fecha de la mision</Label>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="outline">
+                <span v-if="dias"> 
+                  {{dias.day}} / {{ dias.month }} / {{ dias.year }}
+                </span>
+
+                <span v-else> 
+                  Selecciona una fecha 
+                </span>
+
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent>  
+              <Calendar v-model="dias"/>
+            </PopoverContent>
+          </Popover>
+
+        </div>
+
+        <Button
+        type="submit"
+        class="w-full bg-[rgb(106,90,205)] hover:bg-[rgb(88,75,171)] text-white text-md mt-4"
+        >
+          Enviar solicitud
+        </Button>
+
+            
+      </form>
+    </div>
 
   </div>
 </section>
@@ -168,17 +192,17 @@ const dias = ref<DateValue> ()
           <p class="flex items-center gap-2 justify-center md:justify-start">
 
             <Phone class="w-5 h-5 "/>
-            +1 (555) 123-4567
+            + 735 918 385 
 
           </p>
           <p class="flex items-center gap-2 justify-center md:justify-start">
             <Mail class="w-5 h-5 "/>
-            batman@wayneenterprises.com
+            msatsan@gmail.com
 
           </p>
           <p class="flex items-center gap-2 justify-center md:justify-start">
             <MapPin class="w-5 h-5 "/>
-            Wayne Manor, Gotham City
+            Alcoy, Alicante
 
           </p>
         </div>
